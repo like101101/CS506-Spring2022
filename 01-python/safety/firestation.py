2c9526361d9ec3f69eac8c2190a71dfe4628e8e5
Ke Li <keli2023@bu.edu>
2022-02-01 13:13:04 -0500

    Implement hospital, policestation, and firestation

--- 01-python/safety/firestation.py
+++ 01-python/safety/firestation.py
@@ -1,3 +1,25 @@
+from asyncio import events
+from tkinter import font
+from PIL import Image, ImageDraw
+import PySimpleGUI as sg
+import os
+import tkinter
+import io
+
+
 def draw_firestation():
-    print("fire station not found")
-    return
+    img = Image.open("fire.jpeg")
+    img.thumbnail((1200,800))
+    bio = io.BytesIO()
+    img.save(bio, format="PNG")
+
+    layout = [[sg.Text("FireStation", font = ("Arial", 25))],[sg.Image(data = bio.getvalue())], [sg.Button("Don't show anymore")]]
+    window = sg.Window("Image", layout, size = (1200,800))
+
+    while True:
+        event, values = window.read()
+        if event == "Don't show anymore" or event == sg.WIN_CLOSED:
+            break
+
+    window.close()
+
--- 01-python/safety/hospital.py
+++ 01-python/safety/hospital.py
@@ -1,3 +1,28 @@
+
+from asyncio import events
+from tkinter import font
+from PIL import Image, ImageDraw
+import PySimpleGUI as sg
+import os
+import tkinter
+import io
+
+
 def draw_hospital():
-    print("hospital not found")
-    return
+    img = Image.open("hospital.jpeg")
+    img.thumbnail((1200,800))
+    bio = io.BytesIO()
+    img.save(bio, format="PNG")
+
+    layout = [[sg.Text("Hospital", font = ("Arial", 25))],[sg.Image(data = bio.getvalue())], [sg.Button("Don't show anymore")]]
+    window = sg.Window("Image", layout, size = (1200,800))
+
+    while True:
+        event, values = window.read()
+        if event == "Don't show anymore" or event == sg.WIN_CLOSED:
+            break
+
+    window.close()
+
+
+   
